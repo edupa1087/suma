1b844720422140e093611c029a8500f05bab8535
edupa1087 <edupa1087@gmail.com>
2026-07-02 00:07:43 -0500

    Initial commit

---
+++ suma.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Título y diseño
+st.title("Calculadora Básica de Suma")
+st.write("Ingresa dos números para obtener su suma:")
+
+# Entradas de usuario
+num1 = st.number_input("Primer número", value=0)
+num2 = st.number_input("Segundo número", value=0)
+
+# Botón y lógica
+if st.button("Sumar"):
+    resultado = num1 + num2
+    st.success(f"El resultado es: {resultado}")
